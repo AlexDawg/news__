47ab0c186bd28bd7e5e8c672ca2c47c3c2b18f3e
AlexDawg <130207177+AlexDawg@users.noreply.github.com>
2023-05-22 23:02:02 +0700

    Delete NEWS directory

--- NEWS/NewsPortal/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Post, PostCategory, Category, CategorySubscribe
-
-admin.site.register(Post)
-admin.site.register(Category)
-admin.site.register(CategorySubscribe)
-# Register your models here.
